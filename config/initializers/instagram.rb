f86eddbf695ad7b10d4ea5a0e7c13d17382a3a9f
Romain Butteaud <romain.butteaud@gmail.com>
2014-03-19 00:22:24 +0100

    Removing secrets

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,5 +4,5 @@ CALLBACK_URL = "http://localhost:3000/oauth/callback"
 
 Instagram.configure do |config|
 	config.client_id = "5074cf1fbb9f4311af323e08968b90dd"
-	config.client_secret = "1ea9bd3ec71843a7a54e7de88d6a58de"
+	config.client_secret = ENV['SECRET_TOKEN']
 end
